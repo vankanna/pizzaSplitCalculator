0e319bcda578ccba88ecf4c506ae87a9b2cc4fca
vankanna <50307775+vankanna@users.noreply.github.com>
2020-10-28 20:08:05 -0500

    Update code.js

--- code.js
+++ code.js
@@ -19,16 +19,6 @@ function calculatePizzaCost()
 
 }
 
-// prompt for amount of pizza
-// convert to number
-// prompt for amount of topping
-// convert to number
-
-// calculate total cost of pizza(with topping) order
-// alert total cost of pizza
-
-// prompt for number of people ordering together
-// convert to number
-// calculate(divide) cost per one person
-// alert cost per one person
-
+$( document ).ready(function() {
+    $( "#calculate" ).click(calculatePizzaCost);
+});
